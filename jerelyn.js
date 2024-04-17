a3b5d5efd3ebded13d253b30eccce0016c0c1a13
jerelynI <144715038+jerelynI@users.noreply.github.com>
2024-04-17 12:11:50 -0400

    Update jerelyn.js

--- jerelyn.js
+++ jerelyn.js
@@ -1,1 +1,11 @@
-
+//loader
+window.addEventListener('loaf', ()=> {
+  const loader = document.querySelector('.loader');
+  setTimeout(()=> {
+    loader.setAttribute('style', 'opacity: 0,');
+  },500);
+  setTimeout(() => {
+    loader.parentNode.removeChild(loader);
+    document.bodt.style.overflow = 'initial';
+  }, 1000);
+});
